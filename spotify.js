bfd84121b11406a084e46b2b9777ad4caac2170c
prateek1833 <116953515+prateek1833@users.noreply.github.com>
2024-03-01 22:02:41 +0530

    Add files via upload

---
+++ spotify.js
@@ -0,0 +1,18 @@
+let quater=654;
+switch(quater){
+    case 1:
+        console.log("jan,Feb,Mar");
+        break;
+    case 2:
+        console.log("apr,may,june");
+        break;
+    case 3:
+        console.log("jly,may,june");
+        break;
+    case 4:
+        console.log("oct,nov,dec");
+        break;
+    default:
+        console.log("NOOOOOOOOOOOOO");
+
+}
